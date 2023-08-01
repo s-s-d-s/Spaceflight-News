5076fc23845ae9cf7049acef74db72fa390055a7
ssds <dorosh.kostya65@gmail.com>
2023-08-01 12:18:55 +0300

    Updated styles & fixed module imports

--- src/app/module/articles/articles.module.ts
+++ src/app/module/articles/articles.module.ts
@@ -2,25 +2,21 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ArticlesRoutingModule } from "@app/module/articles/articles-routing.module";
 import { ArticlesComponent } from "@app/module/articles/articles.component";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
 import { ArticleCardsModule } from "@app/components/article-cards/article-cards.module";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   imports: [
     CommonModule,
     ArticlesRoutingModule,
-    MatCardModule,
-    MatButtonModule,
+    ArticleCardsModule,
     MatFormFieldModule,
-    MatInputModule,
     MatIconModule,
     MatDividerModule,
-    ArticleCardsModule,
+    MatInputModule,
   ],
   declarations: [
     ArticlesComponent,
